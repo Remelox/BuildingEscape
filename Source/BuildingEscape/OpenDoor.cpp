78da752efb945259049d086eec5216265c5cd157
Adam R. McConnell <remelox@remelox.com>
2018-01-07 17:01:37 -0600

    After doors were added.

---
+++ Source/BuildingEscape/OpenDoor.cpp
@@ -0,0 +1,43 @@
+// Copyright Adam R. McConnell 2018
+
+#include "OpenDoor.h"
+#include "GameFramework/Actor.h"
+
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+	AActor *Owner = GetOwner();
+	FString ObjectName = GetOwner()->GetName();
+	
+	FRotator ObjectRotation = FRotator(0.0f, 90.0f, 0.0f);
+
+	Owner->SetActorRotation(ObjectRotation, ETeleportType::None);
+
+	FString ObjectRotationString = GetOwner()->GetActorRotation().ToString();
+
+	UE_LOG(LogTemp, Warning, TEXT("%s is at %s."), *ObjectName, *ObjectRotationString);
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
