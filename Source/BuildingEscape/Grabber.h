aecb4811960002d33ae82c76a4f39f77e7962124
Adam R. McConnell <remelox@remelox.com>
2018-01-14 15:03:36 -0600

    Added Ray Trace for Grabber.

---
+++ Source/BuildingEscape/Grabber.h
@@ -0,0 +1,33 @@
+// Copyright Adam R. McConnell 2018
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "DrawDebugHelpers.h"
+#include "Grabber.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class BUILDINGESCAPE_API UGrabber : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UGrabber();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+private:
+	float Reach = 100.f;
+
+		
+	
+};
